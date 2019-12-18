3ebbb150dd628e6e6ef7b03fba433ba0c828f86b
HaoruiPeng <haorui.peng@eit.lth.se>
2019-12-18 12:23:31 +0100

    remove old running scripts

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 main.py \
-                --urllc_node 90 --mu 2.99 \
-                --ratio 1.0 --deadline_var 0 \
-                --period_var 0.075 --variance_var 0.25 \
-                --seed 7774394
-
