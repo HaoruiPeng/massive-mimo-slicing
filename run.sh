adc4c6f39e442f76023695c915b5987aba3d5c16
HaoruiPeng <haorui.peng@eit.lth.se>
2020-01-10 17:38:22 +0100

    shorter simulation

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python3 main.py \
-                --urllc_node 90 --mu 2.99 \
-                --ratio 1.0 --deadline_var 0 \
-                --period_var 0.075 --variance_var 0.25 \
-                --seed 7774394
+                --urllc_node 90 --mu 3.12 \
+                --ratio 1.5444444444444445 --deadline_var 0.1 \
+                --period_var 0.06666666666666667 --variance_var 0.06444444444444444\
+                --seed 1025
