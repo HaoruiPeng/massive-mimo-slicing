e4b02dcb3e2b87d95a2e5d2b89691c5aa5c421f5
HaoruiPeng <haorui.peng@eit.lth.se>
2020-02-12 20:01:54 +0100

    modify the run simulation scripts

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python3 main.py \
-          --no_file 48 --no_periodic 0\
+          --no_file 24 --no_periodic 0\
           --period 0.5 --variance 0.02 \
           --d_on 100.0 --d_off 300.0\
-          --seed 1025 --mu 9.3
+          --seed 1025 --mu 3.12
--- run_simulation.py
+++ run_simulation.py
@@ -12,8 +12,6 @@ simulations = []
 
 SEED = round(time.time() / 10)
 
-s2 = "FCFS"
-s1 = "FCFS"
 #
 # period_vars = np.linspace(0.01, 0.15, 15)
 # deadline_vars = np.linspace(0.01, 0.15, 15)
@@ -34,20 +32,21 @@ s1 = "FCFS"
 #                                         --urllc_node {} \
 #                                         --mu {} --seed {}".format(ratio, deadline_var, period_var, variance_var, no_urllc, mu, SEED))
 mu_list = [0, 3.12, 6.9 ,9.3]
-ratios = [1.0]
-no_urllc = 180
-period_var = 0.04
-deadline_var = 0.02
-variance_var = 0.08
+no_file_list = [24, 48]
+no_periodic = 0
+period = 0.5
+variance = 0.02
+d_on = 100.0
+d_off = 300.0
 SEED = 1025
 
-for ratio in ratios:
+for no_file in no_file_list:
     for mu in mu_list:
         simulations.append("python3 main.py \
-                            --ratio {} --deadline_var {} \
-                            --period_var {} --variance_var {} \
-                            --urllc_node {} \
-                            --mu {} --seed {}".format(ratio, deadline_var, period_var, variance_var, no_urllc, mu, SEED))
+                            --no_file {} --no_periodic {} \
+                            --period {} --variance {} \
+                            --d_on {} --d_off {} \
+                            --mu {} --seed {}".format(no_file, no_periodic, period, variance, d_on, d_off, mu, SEED))
 
 pool = Pool(processes=PROCESSES)
 for k, simulation in enumerate(simulations):
